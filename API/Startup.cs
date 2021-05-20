a8fec0914957a1a0d66376bc6d21cde95c8c88fa
bjohnson <bjohnson@talisys.com>
2021-05-20 12:30:21 -0700

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +63,8 @@ namespace API
 
             // allows browser to get from /WeatherForecast endpoint to the WeatherForecast controller
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); // placement of this after routing and before autorization is important
 
             app.UseAuthorization();
 
