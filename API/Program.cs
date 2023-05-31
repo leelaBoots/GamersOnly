8e8f373ec23366bcfdea28fc92581bd474360b8d
bjohnson <bjohnson@talisys.com>
2023-05-30 17:11:47 -0700

    End of lesson 256. Switching to GitHub

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,33 @@ builder.Services.AddSignalR();
 
 //Config.EnableCors(cors);
 
+var connString = "";
+if (builder.Environment.IsDevelopment()) 
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else 
+{
+// Use connection string provided at runtime by FlyIO.
+  var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+  // Parse connection URL to connection string for Npgsql
+  connUrl = connUrl.Replace("postgres://", string.Empty);
+  var pgUserPass = connUrl.Split("@")[0];
+  var pgHostPortDb = connUrl.Split("@")[1];
+  var pgHostPort = pgHostPortDb.Split("/")[0];
+  var pgDb = pgHostPortDb.Split("/")[1];
+  var pgUser = pgUserPass.Split(":")[0];
+  var pgPass = pgUserPass.Split(":")[1];
+  var pgHost = pgHostPort.Split(":")[0];
+  var pgPort = pgHostPort.Split(":")[1];
+  var updatedHost = pgHost.Replace("flycast", "internal");
+
+  connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connString);
+});
+
 // configure the http request pipeline
 
 var app = builder.Build();
@@ -37,14 +64,14 @@ app.UseCors(builder => builder
 app.UseAuthentication();
 app.UseAuthorization();
 
-//app.UseDefaultFiles();
-//app.UseStaticFiles();
+app.UseDefaultFiles();  // finds the index html files from the root folder by default
+app.UseStaticFiles();   // by default, will look for a wwwroot folder then serve content from there
 
 app.MapControllers();
 // SignalR
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
-//app.MapFallbackToController("Index", "Fallback");
+app.MapFallbackToController("Index", "Fallback");
 
 //AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior, true");
 // create a scope for the services that we create here
@@ -65,7 +92,8 @@ try {
     // becareful with this approach, we are modifying database without using Entity Framework
     // this SQL command does not work in SQLite so...
     //await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE [Connections]");
-    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]"); // SQLite
+    //await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]"); // SQLite
+    await Seed.ClearConnections(context);
 
     // seed our database with our json file containing fake data
     await Seed.SeedUsers(userManager, roleManager);
